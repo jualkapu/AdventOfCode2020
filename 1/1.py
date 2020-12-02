ec6f60e07b7cb45e1a0f9b08f802f6c31497f1f8
Juuso Kapulainen <juusokapulainen@Mac.local>
2020-12-02 19:29:37 +0200

    1

---
+++ 1/1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+def input():
+	f = open("input.txt", "r")
+	numbers = []
+	for x in f:
+		numbers.append(int(x))
+	return numbers
+
+def find2(numbers):
+	num = 2020-numbers[0]
+	if num in numbers:
+		return num, numbers[0]
+	else:
+		numbers.pop(0)
+		return find2(numbers)
+
+def find3(numbers):
+	for x in numbers:
+		for y in numbers:
+			if 2020-(x+y) in numbers:
+				return x, y, 2020-(x+y)
+
+
+
+#find TWO entries that sum to 2020 and then multiply those two numbers together.
+x, y = find2(input())
+answer = x*y
+print(answer)
+
+
+#find THREE entries that sum to 2020 and then multiply those two numbers together.
+x, y, z = find3(input())
+answer = x*y*z
+print(answer)
+
+
+
