32af0e964c6555af592873f0a7126f1cb7d7f199
Juuso Kapulainen <juusokapulainen@Mac.local>
2020-12-06 12:26:07 +0200

    Day 6

---
+++ 6/6.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#https://adventofcode.com/2020/day/6/
+
+answer1 = 0
+answer2 = 0
+for x in open("input.txt").read().split('\n\n'):
+	#For answer1: Counts occurences of different chars withi multiple lines
+	letters = set(x.replace('\n', ''))
+	answer1 += len(letters)
+
+	#For answer2: Counts characters that appear on every line within single group
+	passengers = len(x.split('\n'))
+	for y in letters:
+		if x.count(y) == passengers:
+			answer2 += 1
+
+print(answer1)
+print(answer2)
